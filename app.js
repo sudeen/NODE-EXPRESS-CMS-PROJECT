bda0b00dd1bc9e199513c8a3bc8251d8f44e11c7
sudeen <ranjitkarsudeen14@gmail.com>
2019-11-26 18:29:30 +1100

    added admin dashboard link from user nav

--- app.js
+++ app.js
@@ -6,12 +6,17 @@ const exphbs = require("express-handlebars");
 
 app.use(express.static(path.join(__dirname, "public")));
 
+// Set View Engine
 app.engine("handlebars", exphbs({ defaultLayout: "home" }));
 app.set("view engine", "handlebars");
 
+// Load Routes
 const main = require("./routes/home/main");
+const mainAdmin = require("./routes/admin/main-admin");
 
+// Use Routes
 app.use("/", main);
+app.use("/admin", mainAdmin);
 
 const port = process.env.PORT || 8000;
 

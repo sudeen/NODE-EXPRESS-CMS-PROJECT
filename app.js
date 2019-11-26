47fadf015b9d3fda76663de40bc053727f614605
sudeen <ranjitkarsudeen14@gmail.com>
2019-11-26 19:03:11 +1100

    database connection added

--- app.js
+++ app.js
@@ -3,6 +3,14 @@ require("dotenv").config();
 const app = express();
 const path = require("path");
 const exphbs = require("express-handlebars");
+const mongoose = require("mongoose");
+
+mongoose
+  .connect("mongodb://localhost:27017/cms")
+  .then(db => {
+    console.log("MONGO DB CONNECTED");
+  })
+  .catch(error => console.log(error));
 
 app.use(express.static(path.join(__dirname, "public")));
 
@@ -13,10 +21,12 @@ app.set("view engine", "handlebars");
 // Load Routes
 const main = require("./routes/home/main");
 const mainAdmin = require("./routes/admin/main-admin");
+const posts = require("./routes/admin/posts");
 
 // Use Routes
 app.use("/", main);
 app.use("/admin", mainAdmin);
+app.use("/admin/posts", posts);
 
 const port = process.env.PORT || 8000;
 

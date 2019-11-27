a2a2cb6846f9e710afdd53a14d96f8abd52c6e74
sudeen <ranjitkarsudeen14@gmail.com>
2019-11-27 20:30:03 +1100

    flash messages integrated

---
+++ models/Posts.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const PostSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  status: {
+    type: String,
+    default: "public",
+  },
+  allowComments: {
+    type: Boolean,
+    require: true,
+  },
+  body: {
+    type: String,
+    require: true,
+  },
+  file: {
+    type: String
+  },
+});
+
+module.exports = mongoose.model("posts", PostSchema);
---
+++ routes/admin/posts.js
@@ -0,0 +1,113 @@
+const express = require("express");
+const router = express.Router();
+const Post = require("../../models/Posts");
+const { isEmpty, uploadDir } = require("../../helpers/upload-helper");
+const fs = require("fs");
+// const path = require("path");
+
+router.all("/*", (req, res, next) => {
+  req.app.locals.layout = "admin";
+  next();
+});
+
+router.get("/", (req, res) => {
+  Post.find({})
+    .then(posts => {
+      res.render("admin/posts", { posts: posts });
+    })
+    .catch(error => console.log("Cannot get all the posts"));
+});
+
+router.get("/create", (req, res) => {
+  res.render("admin/posts/create");
+});
+
+router.post("/create", (req, res) => {
+  let errors = [];
+
+  if (!req.body.title) {
+    errors.push({ message: "please add a title" });
+  }
+
+  if (!req.body.body) {
+    errors.push({ message: "please add a description" });
+  }
+
+  if (errors.length > 0) {
+    res.render("admin/posts/create", {
+      errors: errors,
+    });
+  } else {
+    let fileName = "LAMBORGINI.jpg";
+
+    if (!isEmpty(req.files)) {
+      let file = req.files.file;
+      fileName = Date.now() + "-" + file.name;
+
+      file.mv("./public/uploads/" + fileName, err => {
+        if (err) throw err;
+      });
+    }
+
+    let allowComments = true;
+    if (req.body.allowComments) {
+      allowComments = true;
+    } else {
+      allowComments = false;
+    }
+    const newPost = new Post({
+      title: req.body.title,
+      status: req.body.status,
+      allowComments: allowComments,
+      body: req.body.body,
+      file: fileName,
+    });
+    newPost
+      .save()
+      .then(savedPost => {
+        // console.log(savedPost);
+        req.flash("success_message", "Post was created successfully");
+        res.redirect("/admin/posts");
+      })
+      .catch(error => console.log("Could not save the post"));
+  }
+});
+
+router.get("/edit/:id", (req, res) => {
+  Post.findById({ _id: req.params.id })
+    .then(post => {
+      console.log("POST", post);
+      res.render("admin/posts/edit", { post: post });
+    })
+    .catch(error => console.log("Cannot get all the posts"));
+});
+
+router.put("/edit/:id", (req, res) => {
+  Post.findOne({ _id: req.params.id }).then(post => {
+    if (req.body.allowComments) {
+      allowComments = true;
+    } else {
+      allowComments = false;
+    }
+    post.title = req.body.title;
+    post.status = req.body.status;
+    post.allowComments = allowComments;
+    post.body = req.body.body;
+    post.save().then(updatedPost => {
+      res.redirect("/admin/posts");
+    });
+  });
+});
+
+router.delete("/:id", (req, res) => {
+  Post.findOne({ _id: req.params.id }).then(post => {
+    fs.unlink(uploadDir + post.file, err => {
+      post.deleteOne();
+      // console.log("error while deleting", err);
+      // req.flash("Success message", "Post successfully deleted");
+      res.redirect("/admin/posts");
+    });
+  });
+});
+
+module.exports = router;

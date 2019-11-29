1cea24ec62d7bf47e7d771ac03a1007275196fab
sudeen <ranjitkarsudeen14@gmail.com>
2019-11-29 19:41:31 +1100

    User model created and register phase started

--- routes/home/index.js
+++ routes/home/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const Post = require("../../models/Posts");
 const Category = require("../../models/Category");
+const User = require("../../models/User");
 
 router.all("/*", (req, res, next) => {
   req.app.locals.layout = "home";
@@ -28,6 +29,48 @@ router.get("/login", (req, res) => {
 
 router.get("/register", (req, res) => {
   res.render("home/register");
+});
+
+router.post("/register", (req, res) => {
+  let errors = [];
+
+  if (!req.body.firstName) {
+    errors.push({ message: "please add a First Name" });
+  }
+
+  if (!req.body.lastName) {
+    errors.push({ message: "please add a Last Name" });
+  }
+
+  if (!req.body.email) {
+    errors.push({ message: "please add an email" });
+  }
+  if (!req.body.password) {
+    errors.push({ message: "please add a passoword" });
+  }
+  if (!req.body.passwordConfirm) {
+    errors.push({ message: "This field cannot be empty" });
+  }
+
+  if (req.body.password !== req.body.passwordConfirm) {
+    errors.push({ message: "Passwords don't match" });
+  }
+
+  if (errors.length > 0) {
+    res.render("home/register", {
+      errors: errors,
+    });
+  } else {
+    let newUser = new User({
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      password: req.body.password,
+    });
+    newUser.save().then(savedUser => {
+      res.send("User was saved");
+    });
+  }
 });
 
 router.get("/post/:id", (req, res) => {

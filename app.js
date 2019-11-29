42c44f5e808363c0fabf7317b485501d80dd9e1b
sudeen <ranjitkarsudeen14@gmail.com>
2019-11-29 22:09:12 +1100

    user login phase with validaiton completed

--- app.js
+++ app.js
@@ -67,6 +67,7 @@ app.use((req, res, next) => {
   res.locals.user = req.user || null;
   res.locals.success_message = req.flash("success_message");
   res.locals.error_message = req.flash("error_message");
+  res.locals.error = req.flash("error");
   next();
 });
 
